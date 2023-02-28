6d4983b3d8c81945bdbfb5a133b5361cc749a115
dzala131 <zaladhr@gmail.com>
2023-02-28 14:35:58 +0530

    mongo.js changed

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+
+let a = 10;
 const connectMongo = async () =>
   mongoose.connect(
     "mongodb+srv://abhikhant:3kEhvE72nNYottdQ@cluster12.xaslabc.mongodb.net/zala?retryWrites=true&w=majority"
